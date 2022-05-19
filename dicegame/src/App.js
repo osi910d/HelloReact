9b20bd0c6732063ce44d38fd284e2fe66e830275
is_ock <osi910d@gmail.com>
2022-05-19 15:24:48 +0900

    Add Components

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,9 +1,14 @@
+import Button from './Button';
 import Dice from './Dice';
 
 
 function App() {
   return (
     <div>
+      <div>
+        <Button text="던지기" />
+        <Button text="처음부터" />
+      </div>
       <Dice color="red" num={2} />
     </div>
   );
